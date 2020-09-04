4333f9a684f2214207d9c7bd8d4a6c02b229e609
Bruno Bach <42494225+brunobach@users.noreply.github.com>
2020-09-04 14:27:03 -0300

    Adicionando Cors-anywhere

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://bibliotecacic.static.delivery/'
+    baseURL: 'https://desolate-forest-52637.herokuapp.com/https://bibliotecacic.static.delivery/',
 })
 
 export default api;
